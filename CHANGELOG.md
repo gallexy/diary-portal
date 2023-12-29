75131160e2260365be33a7dba70b17a4f00184a4
liu xiaohua <liuxiaohua72@yahoo.com>
2023-12-29 11:46:55 +0800

    Update .gitignore to ignore config directory

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,5 +8,7 @@
 
 ### 创建 docker-ready 分支
 -把utility.js从config目录剥离到lib目录，config目录只保留需要的配置信息，启动docker时用户可以提供自己的配置
+
 -mysql更新到mysql2,避免认证错误
+
 -增加Dockerfile和docker-compose.yml，用户可以以docker方式启动服务，甚至可以制作自己的docker镜像
--- config/configDatabase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host:       'diary.gallexy.icu',
-    user:       'admin',
-    password:   'lxh12345',
-    port:       '3306',
-    multipleStatements: true, // 允许同时请求多条 sql 语句
-    timezone: ''
-}
